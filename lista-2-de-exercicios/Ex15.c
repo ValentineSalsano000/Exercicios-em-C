3ff17c43838925b9931edf1b8338b3bd32c213b4
ValentineSalsano <vsalsano8@gmail.com>
2025-11-30 20:20:01 -0300

    Ex15 e Ex16
    enunciado

---
+++ lista-2-de-exercicios/Ex15.c
@@ -0,0 +1,9 @@
+/*
+Ainda em relação ao código do exercício 14: 
+crie um vetor de 10 alunos, inicialmente zerados.
+ Preencha apenas o ano de nascimento de todos eles. 
+ Em seguida calcule a média de idade de todos 
+ e imprima esse resultado (sabendo que 
+
+
+*/
---
+++ lista-2-de-exercicios/Ex16.c
@@ -0,0 +1,6 @@
+/*
+Analise os códigos abaixo. 
+Como a recursão é utilizada no código da esquerda (explique dando exemplo)? 
+Pontue a diferença com o código da direit
+
+*/
